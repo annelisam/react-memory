9e22ca621f3e592ce3e30727b34f0f641ad3bfb0
Annelisa <annelisamm@icloud.com>
2018-05-02 10:57:20 -0700

    Created card details

---
+++ react-memory/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+ } from 'reactstrap';
+
+export default class MyNav extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">button-click</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink className="disabled text-white" href="/">Click an image to begin!</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="disabled text-white" href="/">Score: | Top Score: </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
